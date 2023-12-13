bce5e9a57030b216ac9a8e6dd383ded58525b2ed
Tania Aguirre Lordoguin <taguirre@gmail.com>
2023-12-13 17:04:43 +0100

    draft:create train.py structure

--- train.py
+++ train.py
@@ -18,31 +18,50 @@ def main(arguments):
         validation_size=arguments.validation_size)
 
     for epoch in range(arguments.max_epochs):
-        epoch_loss = 0
+        epoch_loss_supervised = 0
+        epoch_loss_unsupervised = 0
+
         supervised_batch = iter(data_loaders['supervised'])
         unsupervised_batch = iter(data_loaders['unsupervised'])
         # FIXME: Again not considering limits 0 and 1
         batches_per_epoch = len(data_loaders['supervised']) +\
             len(data_loaders['unsupervised'])
+        Tsup = batches_per_epoch // len(data_loaders['supervised'])
 
         model = CCVAE()
-        # In this case we want to iterate in both supervised and unsupervised dataset
+        optimizer = torch.optim.Adam(
+            params=model.parameters(),
+            lr=arguments.learning_rate)
+
+        # In this case we want to train with both supervised  
+        # and unsupervised dataset. The first one is going to
+        # be iterated at a period Tsup
         for i in tqdm(range(batches_per_epoch)):
-            (images_supervised, labels_supervised) = next(supervised_batch)
-            (images_unsupervised, labels_unsupervised) = next(unsupervised_batch)
-
-            # Compute Forward
-
-            # Get loss
-
+            if i % Tsup == 0:
+                (images, labels) = next(supervised_batch)
+                model.supervised(images, labels)
+                loss = model.loss()
+                epoch_loss_supervised+=loss.detach().item()
+            else:
+                (images, labels) = next(unsupervised_batch)
+                model.unsupervised(images, labels)
+                loss = model.loss()
+                epoch_loss_unsupervised+=loss.detach().item()
+            
             # backward
-
+            loss.backward()
             # optimization step
-
-
-
+            optimizer.step()
+            optimizer.zero_grad()
+            # validation
+            with torch.no_grad():
+                validation_accuracy = model.accuracy(
+                    data_loaders['validation'])
+        print(f"[Epoch {epoch}] Sup Loss "
+              f"{epoch_loss_supervised:.3f},"
+              f" Unsup Loss {epoch_loss_unsupervised:.3f},"
+              f" Val Acc {validation_accuracy:.2f}")
     print()
-    
 
 
 if __name__ == "__main__":
