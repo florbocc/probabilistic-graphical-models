36869c2b1ac80b78f9b5e5b2cb11378b97b74250
Tania Aguirre Lordoguin <taguirre@gmail.com>
2023-12-18 22:59:15 +0100

    fix_experiments

--- test.py
+++ test.py
@@ -25,20 +25,20 @@ def main(arguments):
         )
 
     model.load(arguments.model_path)
-
+    model.to(device)
+    model.eval()
     data_loaders = setup_data_loaders(
         files.datasets_folder,
         arguments.supervised_fraction,
         batch_size=arguments.batch_size,
         validation_size=arguments.validation_size)
 
-    model.to(device)
     image, label = next(iter(data_loaders['test']))
     image = image.to(device=device)
     label = label.to(device=device)
     # EXPERIMENTS
-    image_indices = [0, 80, 52, 90]
-
+    image_indices = [0, 80, 52, 90, 89, 63, 50, 87]
+    
     # CONDITIONAL GENERATION
     image_index = 0
     number_of_samples = 4
@@ -46,70 +46,62 @@ def main(arguments):
         image_name = data_loaders['test'].dataset.filename[image_index]
         reconstucted_samples = model.conditional_generation(image[image_index, :], label[image_index, :], num_sample=number_of_samples)
         save_image(
-            image[0, :],
-            os.path.join(arguments.model_path,
+            image[image_index, :],
+            os.path.join(files.output_folder,
             f'original_'+image_name))
 
         save_image(
             make_grid(reconstucted_samples, nrow=number_of_samples),
-            os.path.join(arguments.model_path,
+            os.path.join(files.output_folder,
             f'test_conditional_generation_'+image_name))
 
     # VARIANCE INTERVENING
     for image_index in image_indices:
         image_name = data_loaders['test'].dataset.filename[image_index]
         y = label[image_index, :]
-
-        for i, label_name in enumerate(CELEBA_EASY_LABELS):
+        labels_intervation = image[image_index, :].view(1, im_shape[0], im_shape[1], im_shape[2])
+        for i, label_name in enumerate(['Bangs', 'Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Eyeglasses', 'Male','Young']):
             if y[i] == 1:
-                continue
-            y[i] = 1
-            r = model.conditional_generation(
-                image[image_index, :],
-                y,
-                num_sample=1)
-            y[i] = 0
-            save_image(
-                r,
-                os.path.join(arguments.model_path,
+                r = torch.zeros_like(image[image_index, :], device=device).view(1, im_shape[0], im_shape[1], im_shape[2])
+            else:
+                y[i] = 1
+                r = model.conditional_generation(
+                    image[image_index, :],
+                    y,
+                    num_sample=1)
+                y[i] = 0
+            labels_intervation = torch.cat((labels_intervation, r), axis=0)
+        transform = transforms.Grayscale()
+        dif = torch.round(torch.abs(transform(labels_intervation) - transform(image[image_index, :])))
+        save_image(
+            make_grid(dif, nrow=len(image_indices)),
+            os.path.join(files.output_folder,
+            f'test_intervation_diff_on_label_{label_name}_'+image_name))
+        save_image(
+                make_grid(labels_intervation, nrow=len(image_indices)),
+                os.path.join(files.output_folder,
                 f'test_intervation_on_label_{label_name}_'+image_name))
-            transform = transforms.Grayscale()
-            dif = torch.round(torch.abs(transform(r) - transform(image[image_index, :])))
-            save_image(
-                dif,
-                os.path.join(arguments.model_path,
-                f'test_intervation_diff_on_label_{label_name}_'+image_name))
 
     # CHARACTERISTIC SWAP
-    for image_index in image_indices:
-        image_name_1 = data_loaders['test'].dataset.filename[image_index]
-        image_name_2 = data_loaders['test'].dataset.filename[image_index+1]
-        image_1 = image[image_index, :]
-        image_2 = image[image_index+1, :]
-        z1 = dist.Normal(*model.encoder(image_1)).sample()
-        zc1, zs1 = z1.split([model.num_labeled, model.num_unlabeled], 1)
-        z2 = dist.Normal(*model.encoder(image_2)).sample()
-        zc2, zs2 = z2.split([model.num_labeled, model.num_unlabeled], 1)
-        new_z_1 = torch.cat((zc1, zs2), axis=1)
-        r1 = model.decoder(new_z_1)
-        new_z_2 = torch.cat((zc2, zs1), axis=1)
-        r2 = model.decoder(new_z_2)
+    for i in image_indices:
+        image_name_1 = data_loaders['test'].dataset.filename[i]
+        image_1 = image[i, :]
+        row_recon = image_1.view(1, im_shape[0], im_shape[1], im_shape[2])
+        for j in image_indices:
+            image_name_2 = data_loaders['test'].dataset.filename[j]
+            image_2 = image[j, :]
+            z1 = dist.Normal(*model.encoder(image_1)).sample()
+            zc1, zs1 = z1.split([model.num_labeled, model.num_unlabeled], 1)
+            z2 = dist.Normal(*model.encoder(image_2)).sample()
+            zc2, zs2 = z2.split([model.num_labeled, model.num_unlabeled], 1)
+            new_z_1 = torch.cat((zc1, zs2), axis=1)
+            r1 = model.decoder(new_z_1)
+            row_recon = torch.cat((row_recon, r1), axis=0)
 
         save_image(
-            r1,
-            os.path.join(arguments.model_path,
+            make_grid(row_recon, nrow=len(image_indices)+1),
+            os.path.join(files.output_folder,
             f'test_swap_zc1_zs2_{image_name_2[:-4]}_'+image_name_1))
-        save_image(
-            r2,
-            os.path.join(arguments.model_path,
-            f'test_swap_zc2_zs1{image_name_2[:-4]}_'+image_name_1))
-        
-        save_image(
-            image_2,
-            os.path.join(arguments.model_path,
-            f'test_swap_'+image_name_2))
-        
-        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
